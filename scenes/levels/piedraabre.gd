6b379af01ead4453f4843b38cd6de46a0a60c6a7
tinxo3 <martinalelopez@outlook.com>
2021-12-13 19:11:59 -0300

    cajas funcionales (cambiar item que dropea)

--- scenes/levels/piedraabre.gd
+++ scenes/levels/piedraabre.gd
@@ -3,5 +3,5 @@ extends StaticBody2D
 onready var anims = $animacion
 
 func _on_Area2D_body_entered(body):
-	if Inventory.get_item("un g") > 0:
+	if Inventory.get_item("un g") > 0 and Inventory.get_item("llave") > 0:
 		anims.play("abierta")
