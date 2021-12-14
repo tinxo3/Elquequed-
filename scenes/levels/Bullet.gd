fbc851d72e2cb04d4a0e96af06b898e3947b9832
Juan Andres Viola <juanandresviola@gmail.com>
2021-12-14 17:39:02 -0300

    balas
    desaparecen al hitear

--- scenes/levels/Bullet.gd
+++ scenes/levels/Bullet.gd
@@ -23,5 +23,19 @@ func _physics_process(delta):
 
 func _on_Bullet_body_entered(body):
 	if body.is_in_group("player"):
-		 queue_free()
+		get_parent().remove_child(self)
+		get_node("anims").play("disappear")
+		queue_free()
+	pass # Replace with function body.
+
+func _on_Bullet_area_entered(area):
+	if area.is_in_group("player"):
+		get_parent().remove_child(self)
+		get_node("anims").play("disappear")
+		queue_free()
+	pass # Replace with function body.
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
 	pass # Replace with function body.
